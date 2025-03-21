690c76a79540687dbc09252ba8c023933439a527
leonxcom <leonx.contact@gmail.com>
2025-03-22 02:06:20 +0800

    Refactor: Optimize navbar layout for mobile devices

--- components/icons.tsx
+++ components/icons.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import Image from "next/image";
 
 import { IconSvgProps } from "@/types";
 
@@ -6,22 +7,16 @@ export const Logo: React.FC<IconSvgProps> = ({
   size = 36,
   width,
   height,
-  ...props
+  ..._props
 }) => (
-  <svg
-    fill="none"
-    height={size || height}
-    viewBox="0 0 32 32"
-    width={size || width}
-    {...props}
-  >
-    <path
-      clipRule="evenodd"
-      d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
-      fill="currentColor"
-      fillRule="evenodd"
-    />
-  </svg>
+  <Image
+    priority
+    alt="Nostudy.ai Logo"
+    height={Number(size || height || 36)}
+    src="/logo.png"
+    style={{ objectFit: "contain" }}
+    width={Number(size || width || 36)}
+  />
 );
 
 export const DiscordIcon: React.FC<IconSvgProps> = ({
@@ -158,16 +153,21 @@ export const HeartFilledIcon = ({
   </svg>
 );
 
-export const SearchIcon = (props: IconSvgProps) => (
+export const SearchIcon = ({
+  size = 24,
+  width,
+  height,
+  ...otherProps
+}: IconSvgProps) => (
   <svg
     aria-hidden="true"
     fill="none"
     focusable="false"
-    height="1em"
+    height={size || height || "1em"}
     role="presentation"
     viewBox="0 0 24 24"
-    width="1em"
-    {...props}
+    width={size || width || "1em"}
+    {...otherProps}
   >
     <path
       d="M11.5 21C16.7467 21 21 16.7467 21 11.5C21 6.25329 16.7467 2 11.5 2C6.25329 2 2 6.25329 2 11.5C2 16.7467 6.25329 21 11.5 21Z"

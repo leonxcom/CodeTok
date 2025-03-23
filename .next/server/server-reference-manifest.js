887d28b7fed32e23bcdb3977b4d05f124754bca2
leohuangdev <leohuangdev@gmail.com>
2025-03-23 14:07:51 +0800

    Add Magic UI components

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"gn6HklH7A4pjTO1OBH1+nSuPX3RR+71v+kQSglCUDfE=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
--- src/db/schema/index.ts
+++ src/db/schema/index.ts
@@ -1,2 +1,4 @@
 // Export all schema definitions
 export * from './users'
+export * from './projects'
+export * from './learning-paths'

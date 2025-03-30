bfc9668325a56c1de0bc616b643ac40577d92de3
leonxcom <leonx.contact@gmail.com>
2025-03-30 16:06:00 +0800

    chore: remove deprecated files and configurations

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-import createNextIntlPlugin from 'next-intl/plugin'
-
-// 指定i18n request配置文件的路径
-const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts')
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-export default withNextIntl(nextConfig)
--- src/components/auth/index.ts
+++
@@ -1,5 +0,0 @@
-// Re-export all auth components
-export { SignInForm } from './SignInForm'
-export { SignUpForm } from './SignUpForm'
-export { VerifyEmailForm } from './VerifyEmailForm'
-export { PasswordResetForm } from './PasswordResetForm'

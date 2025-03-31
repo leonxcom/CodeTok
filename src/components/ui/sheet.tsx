deb283ac5b20c267708bc25502087554f9b6d5cf
leohuangdev <leohuangdev@gmail.com>
2025-04-01 00:23:40 +0800

    Update sidebar: Change 'MENU' to 'Navigation' and reduce sidebar width

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -45,9 +45,9 @@ const SheetContent = React.forwardRef<
       className={cn(
         "fixed z-50 gap-4 bg-background p-6 shadow-lg transition ease-in-out data-[state=open]:animate-in data-[state=closed]:animate-out",
         {
-          "inset-y-0 left-0 h-full w-3/4 sm:w-1/3 border-r data-[state=closed]:slide-out-to-left data-[state=open]:slide-in-from-left":
+          "inset-y-0 left-0 h-full w-[250px] sm:w-[250px] border-r data-[state=closed]:slide-out-to-left data-[state=open]:slide-in-from-left":
             side === "left",
-          "inset-y-0 right-0 h-full w-3/4 sm:w-1/3 border-l data-[state=closed]:slide-out-to-right data-[state=open]:slide-in-from-right":
+          "inset-y-0 right-0 h-full w-[250px] sm:w-[250px] border-l data-[state=closed]:slide-out-to-right data-[state=open]:slide-in-from-right":
             side === "right",
           "inset-x-0 top-0 h-auto border-b data-[state=closed]:slide-out-to-top data-[state=open]:slide-in-from-top":
             side === "top",
